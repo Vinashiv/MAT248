7595a125f0545f3b0da3c5d55380fed134d0f393
aagams2Shah <111478880+aagams2Shah@users.noreply.github.com>
2022-10-10 21:55:17 +0530

    Add files via upload
    Version 2

--- Assignment1.py
+++ Assignment1.py
@@ -1,5 +1,13 @@
-
-
+def generateIdentityeMatrix(size):
+        m = Matrix(size,size)
+        for i in range(size):
+            for j in range(size):
+                if(i==j):
+                    m.Rows[i][j] = 1
+                else:
+                    m.Rows[i][j] = 0
+                    
+        return m
 class Matrix():
     def __init__(self,numRows,numCols):
         self.Rows=[[None]*numCols for i in range(numRows)]
@@ -9,6 +17,15 @@ class Matrix():
     
     def numofCols(self):
         return len(self.Rows[0])
+
+    def copyMatrix(self):
+        n = self.numofCols()
+        n1 = self.numOfRows()
+        m = Matrix(n1,n)
+        for i in range(n1):
+            for j in range(n):
+                m.Rows[i][j] = self.Rows[i][j]
+        return m
     
     def printMatrix(self):
         for i in range(self.numOfRows()):
@@ -41,20 +58,61 @@ class Matrix():
         for i in range(n):
             prod*=m.Rows[i][i]
         return abs(prod)
-
     
+    def inverse(self):
+        assert self.numofCols() == self.numOfRows(),"Given matrix is not a Square Matrix"
+        assert self.FindDeterminant()!=0,"Determinant of the matrix is zero"
+        n = self.numofCols()
+        m = self.copyMatrix()
+        m1 = generateIdentityeMatrix(n)
+        pivot = 0
+        for row in range(n):
+            if(n<=pivot):
+                break
+            i = row
+            while(m.Rows[i][pivot]==0):
+                i+=1
+                if(i==row):
+                    pivot+=1
+                if(n==pivot):
+                    break
+            m.Rows[i],m.Rows[row] = m.Rows[row],m.Rows[i]
+            m1.Rows[i],m1.Rows[row] = m1.Rows[row],m1.Rows[i]
+            c = m.Rows[row][pivot]
+            for j in range(n):
+                m.Rows[row][j] = m.Rows[row][j]/c
+                m1.Rows[row][j] = m1.Rows[row][j]/c
+                
+            for k in range(n):
+                if(k!=row):
+                    c1 = m.Rows[k][pivot]
+                    for j in range(n):
+                        m.Rows[k][j] = m.Rows[k][j] - c1*m.Rows[pivot][j]
+                        m1.Rows[k][j] = m1.Rows[k][j] - c1*m1.Rows[pivot][j]
+                        
+            pivot+=1
+        return m1
+
+    def setMatrix(self):
+        n = self.numofCols()
+        for i in range(n):
+            for j in range(n):
+                k = int(input())
+                self.Rows[i][j] = k
+
+        
 
 m = Matrix(3,3)
-c=1
-for i in range(3):
-    for j in range(3):
-        m.Rows[i][j] = c
-        c+=1
+m.setMatrix()
 
 m.printMatrix()
+print()
+m1=m.inverse()
+m1.printMatrix()
 
-det = m.FindDeterminant()
-print(det)
+
+
+
 
  
 
